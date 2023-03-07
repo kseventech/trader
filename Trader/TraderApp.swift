fb8fc3b720a442a768d672f169e4d7c2a031a6dc
deltakroneker <nikola.milic.ios@gmail.com>
2023-03-07 14:32:15 +0100

    Initial Commit

---
+++ Trader/TraderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraderApp.swift
+//  Trader
+//
+//  Created by nikolamilic on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct TraderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
